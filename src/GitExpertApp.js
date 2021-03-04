07a9e34dc66dc83af45b8e95564c850a365befb4
Eduardo Francos Margareto <efmargareto@hotmail.com>
2021-03-04 20:48:20 +0000

    First commit

---
+++ src/GitExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+
+const GifExpertApp = () => {
+
+    const [ categorias, setCategorias ] = useState(['Basketball']) // []
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategorias={ setCategorias }/>
+
+            <hr />
+
+            <ol>
+                {
+                    categorias.map( category => (
+
+                        <GifGrid 
+                            key={ category }    
+                            category={ category }
+                        />
+
+                    ))
+                }
+            </ol>
+
+        </>
+    )
+}
+
+export default GifExpertApp
